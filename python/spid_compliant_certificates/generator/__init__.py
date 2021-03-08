1dfad578f0dd01f9ae3e75e3f218ff44395d8cf0
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2021-03-08 23:30:21 +0100

    Refactoring

--- python/spid_compliant_certificates/generator/__init__.py
+++ python/spid_compliant_certificates/generator/__init__.py
@@ -17,3 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+from spid_compliant_certificates.generator.generate import generate
+
+__all__ = ['generate']
